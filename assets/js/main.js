6a955dfabb36d8392f44f6456188247202cc9be9
3DUTEX <t3xdev@gmail.com>
2023-08-19 20:43:56 -0300

    Responsivo no celular e resultado modificado

--- assets/js/main.js
+++ assets/js/main.js
@@ -24,39 +24,68 @@ function meuEscopo() {
     }
   }
 
+  let primeiraVez = true;
   //exibe resultado
   function displayResult() {
-    if (imc < 18.5) {
-      resultado.textContent = `Seu imc é: ${imc} (Abaixo do peso)`;
-    } else if (imc >= 18.5 && imc <= 24.9) {
-      resultado.textContent = `Seu imc é: ${imc} (Peso normal)`;
-    } else if (imc >= 25 && imc <= 29.9) {
-      resultado.textContent = `Seu imc é: ${imc} (Sobrepeso)`;
-    } else if (imc >= 30 && imc <= 34.9) {
-      resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 1)`;
-    } else if (imc >= 35 && 39.9) {
-      resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 2)`;
-    } else {
-      resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 3)`;
-    }
+    //se for a primeira vez
+    if (primeiraVez) {
+      if (imc < 18.5) {
+        resultado.textContent = `Seu imc é: ${imc} (Abaixo do peso)`;
+      } else if (imc >= 18.5 && imc <= 24.9) {
+        resultado.textContent = `Seu imc é: ${imc} (Peso normal)`;
+      } else if (imc >= 25 && imc <= 29.9) {
+        resultado.textContent = `Seu imc é: ${imc} (Sobrepeso)`;
+      } else if (imc >= 30 && imc <= 34.9) {
+        resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 1)`;
+      } else if (imc >= 35 && 39.9) {
+        resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 2)`;
+      } else {
+        resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 3)`;
+      }
 
-    resultado.style.display = "block";
-    setTimeout(() => {
-      resultado.className = "on";
-    }, 100);
-
-    setTimeout(() => {
-      resultado.className = "off";
+      resultado.style.display = "block";
       setTimeout(() => {
-        resultado.style.display = "none";
-      }, 350);
-    }, 3000);
+        resultado.className = "on";
+      }, 100);
+
+      // setTimeout(() => {
+      //   resultado.className = "off";
+      //   setTimeout(() => {
+      //     resultado.style.display = "none";
+      //   }, 350);
+      // }, 3000);
+
+      //passando a primeira vez
+      primeiraVez = false;
+    } else {
+      resultado.textContent = "Aguarde...";
+      setTimeout(() => {
+        if (imc < 18.5) {
+          resultado.textContent = `Seu imc é: ${imc} (Abaixo do peso)`;
+        } else if (imc >= 18.5 && imc <= 24.9) {
+          resultado.textContent = `Seu imc é: ${imc} (Peso normal)`;
+        } else if (imc >= 25 && imc <= 29.9) {
+          resultado.textContent = `Seu imc é: ${imc} (Sobrepeso)`;
+        } else if (imc >= 30 && imc <= 34.9) {
+          resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 1)`;
+        } else if (imc >= 35 && 39.9) {
+          resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 2)`;
+        } else {
+          resultado.textContent = `Seu imc é: ${imc} (Obesidade grau 3)`;
+        }
+      }, 3450);
+    }
   }
 
   function calcular() {
     if (checkFields()) {
       pesoNumber = Number(peso.value);
       alturaNumber = Number(altura.value);
+
+      if (alturaNumber > 3) {
+        alturaNumber = alturaNumber / 100;
+        altura.value = alturaNumber.toString();
+      }
 
       imc = pesoNumber / Math.pow(alturaNumber, 2);
       imc = imc.toFixed(2);
